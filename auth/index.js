52459f07a5bc486f23e857dd1e2c419326459227
Raine Petersen <raineforest01@gmail.com>
2021-10-19 17:31:37 -0600

    Adder new user constructor

--- auth/index.js
+++ auth/index.js
@@ -1,6 +1,17 @@
 require('dotenv').config();
 const jtw = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const uid = require('uuid').v4;
+
+class User {
+  constructor(firstName, lastName, email, password, token) {
+    (this.uid = uid()),
+      (this.firstName = firstName),
+      (this.lastName = lastName),
+      (this.email = email),
+      (this.password = password);
+  }
+}
 
 const registerUser = async (user) => {
   try {
