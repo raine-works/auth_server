1fa610b3ce9ff63ba3c97107e7e74431ae5b196d
Raine Petersen <raine@rqinspections.com>
2021-10-19 23:59:24 -0600

    Setup user creation and login

--- auth/index.js
+++ auth/index.js
@@ -1,21 +1,145 @@
 require('dotenv').config();
-const jtw = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const uid = require('uuid').v4;
+const { DateTime } = require("luxon");
+const db = require('../database/users.json');
+
+// Hash users password for safe keeping
+const hashPassword = async (password) => {
+  return await bcrypt.hash(password, 10);
+}
 
 class User {
-  constructor(firstName, lastName, email, password, token) {
-    (this.uid = uid()),
-      (this.firstName = firstName),
-      (this.lastName = lastName),
-      (this.email = email),
-      (this.password = password);
+  constructor(firstName, lastName, email, password, ipAddress) {
+    this.uid = uid(),
+    this.firstName = firstName,
+    this.lastName = lastName,
+    this.email = email.toLowerCase(),
+    this.password = password, 
+    this.token = null, 
+    this.createdDate = DateTime.utc(), 
+    this.lastLogin = DateTime.utc(), 
+    this.lastLoginIpAdress = ipAddress
   }
 }
 
-const registerUser = async (user) => {
+// Register a new user
+exports.registerUser = async (event, metadata) => {
   try {
+
+    const { firstName, lastName, email, password } = event;
+    const { ipAddress } = metadata;
+    const encryptedPassword = await hashPassword(password);
+
+    // Look for existing user with the same email in the database
+    const existingUser = db.users.find(u => u.email === email.toLowerCase());
+    if (existingUser) {
+
+      // return response if there is already a user in the database with this email
+      return {
+        status: 400, 
+        data: `${event.email.toLowerCase()} has already been registered.`
+      }
+
+    } else {
+
+      // Create user record in database
+      const newUser = await new User(firstName, lastName, email, encryptedPassword, ipAddress);
+
+      // Generate new token
+      const token = jwt.sign(
+        { uid: newUser.uid, email: newUser.email }, 
+        process.env.TOKEN_KEY, 
+        { expiresIn: '2' }
+      )
+
+      // Save new token to user record
+      newUser.token = token;
+
+      // Add new user record to the database
+      db.users.push(newUser);
+
+      // return new user record with a valid token
+      return {
+        status: 200, 
+        data: {
+          firstName: newUser.firstName, 
+          lastName: newUser.lastName, 
+          email: newUser.email, 
+          uid: newUser.uid, 
+          createdDate: newUser.createdDate, 
+          lastLogin: newUser.lastLogin, 
+          token: newUser.token
+        }
+      }
+
+    }
   } catch (error) {
+
+    // Throw error if something breaks
     throw error;
+
   }
 };
+
+// Verify existing user and authenticate them
+exports.loginUser = async (event, metadata) => {
+  try {
+
+    const { email, password } = event;
+    const { ipAddress } = metadata;
+
+    // Find existing user in database by email
+    const existingUser = db.users.find(u => u.email === email.toLowerCase());
+
+    // If user exists move to the authentication step
+    if (existingUser) {
+
+      // Verify users password and create new token
+      if (await bcrypt.compare(password, existingUser.password)) {
+        const token = jwt.sign(
+          { uid: existingUser.uid, email: existingUser.email }, 
+          process.env.TOKEN_KEY, 
+          { expiresIn: '2h' }
+        )
+
+        existingUser.token = token;
+        existingUser.lastLogin = DateTime.utc();
+        existingUser.lastLoginIpAdress = ipAddress;
+        return {
+          status: 200, 
+          data: {
+            firstName: existingUser.firstName, 
+            lastName: existingUser.lastName, 
+            email: existingUser.email, 
+            uid: existingUser.uid, 
+            createdDate: existingUser.createdDate, 
+            lastLogin: existingUser.lastLogin, 
+            token: existingUser.token
+          }
+        }
+
+      // return invalid password message if password does not match
+      } else {
+        return {
+          status: 400, 
+          data: 'Invalid password'
+        }
+      }
+    
+    // return no user found message 
+    } else {
+      return {
+        status: 400, 
+        data: `${email} has not yet been registered.`
+      } 
+    }
+
+  } catch (error) {
+
+    // Throw error if something breaks
+    throw error;
+
+  }
+}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const Joi = require('joi');
 const express = require('express');
 const server = express();
+const auth = require('./auth/index');
 server.use(express.json());
 
 // Start listening for requests
@@ -10,9 +11,78 @@ server.listen(process.env.PORT, () => {
 });
 
 // Register new users
-server.post('/register', (req, res) => {});
+server.post('/register', async (req, res) => {
+  try {
+    const validRequest = await newUser.validate(req.body);
+    if (!validRequest.error) {
+      const response = await auth.registerUser(validRequest.value, { ipAddress: req.ip });
+      res.status(response.status).json({
+        success: response.status === 200 ? true : false, 
+        data: response.data
+      })
+    } else {
+      res.status(400).json({
+        success: false, 
+        error: {
+          message: 'Validation error', 
+          context: validRequest.error.details
+        }
+      })
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false, 
+      error: {
+        message: 'Internal server error', 
+        context: error.message
+      }
+    })
+  }
+});
 
 // Login existing users
-server.post('/login', (req, res) => {});
+server.post('/login', async (req, res) => {
+  try {
+    const validRequest = await existingUser.validate(req.body);
+    if (!validRequest.error) {
+      const response = await auth.loginUser(validRequest.value, { ipAddress: req.ip });
+      res.status(response.status).json({
+        success: response.status === 200 ? true : false, 
+        data: response.data
+      })
+    } else {
+      res.status(400).json({
+        success: false, 
+        error: {
+          message: 'Validation error', 
+          context: validRequest.error.details
+        }
+      })
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false, 
+      error: {
+        message: 'Internal server error', 
+        context: error.message
+      }
+    })
+  }
+});
 
-// Validate request data
+// Validate request data 
+// (new user schema)
+const newUser = Joi.object({
+  firstName: Joi.string().required(), 
+  lastName: Joi.string().required(), 
+  email: Joi.string().email().required(), 
+  password: Joi.string().required()
+})
+
+// (existing user schema)
+const existingUser = Joi.object({
+  email: Joi.string().email().required(), 
+  password: Joi.string().required()
+})
